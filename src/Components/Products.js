aa8563dd6e427dd3c20c81623f1266c4049c2d29
AaquibSyed <m.nsyedaaquib@gmail.com>
2020-12-03 21:38:27 +0530

    Animation and Modal added

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react'
 import './Products.css'
 import formatCurrency from './../utils';
+import Fade  from 'react-reveal/Fade'
+import Modal from 'react-modal'
 
 export default class Products extends Component {
+    constructor(props){
+        super(props);      
+        this.state={
+            clickedProduct:null
+        }
+    }
+      productClicked = (product)=>{
+          console.log(product)
+          this.setState({
+              clickedProduct:product,
+          })
+      }
+
+      closeModal=()=>{
+        this.setState({
+            clickedProduct:null,
+        })
+      }
     render() {
+        const {clickedProduct} = this.state
         return (
             <div>
+                <Fade bottom cascade duration={1000}>
                 <ul className="products">
                     {this.props.products.map(product=>(
                         <li key={product.id}>
                             <div className="product">
-                                <a href={"#"+product._id}>
+                                <a href={"#"+product._id} onClick={()=>{this.productClicked(product)}}>
                                     <img
                                      className="product_image"
                                      alt={product.title}
@@ -29,6 +51,42 @@ export default class Products extends Component {
                         </li>
                     ))}
                 </ul>
+                </Fade>
+                {
+                    this.state.clickedProduct && (
+                      
+                            <Modal isOpen={true} onRequestClose={this.closeModal} ariaHideApp={false}>
+                                <button
+                                 className='btn-closemodal' onClick={this.closeModal}>X</button>
+                                 <div className="modal_content">
+                                     <div className="modal_logo">
+                                         <img  className="modal_image" src={clickedProduct.image} alt={clickedProduct.title}/>
+                                     </div>
+                                     <div className="modal_details">
+                                           <h2 className="modal_title">{clickedProduct.title}</h2>                                           
+                                            <p><strong>Description : </strong> {clickedProduct.description}</p>
+                                           <p><strong>Price : </strong>{formatCurrency(clickedProduct.price)}</p>
+                                           <div className="modal_sizes">
+                                             <p> <strong>Available Sizes : </strong>
+                                             {
+                                                 clickedProduct.availableSizes?.map((size)=>(
+                                                 <button className="button-sizes" key={size}>{size}</button>
+                                                 ))
+                                             }
+                                             </p>
+                                           </div>
+                                           <button className="btn primary"
+                                           onClick={()=>this.props.addToCart(clickedProduct)}
+                                           >Add to cart</button>
+                                     </div>
+                                 </div>
+                               
+                           </Modal>
+                       
+                        
+
+                    )
+                }
             </div>
         )
     }
